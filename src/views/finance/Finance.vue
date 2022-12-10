f0043736fb09f433b7e0b7ad6f71665d733a88d2
masmuss <ahmusafir.khoirul@gmail.com>
2022-12-10 21:11:41 +0700

    add finance date filter
    Signed-off-by: masmuss <ahmusafir.khoirul@gmail.com>

--- src/views/finance/Finance.vue
+++ src/views/finance/Finance.vue
@@ -1,12 +1,15 @@
 <script>
 import Input from '../../components/Form/Input.vue'
 import Button from '../../components/Form/Button.vue'
-import { onMounted, ref, reactive } from 'vue'
+import { onMounted, watch, ref, reactive } from 'vue'
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
 export default {
 	setup() {
+		let date = new Date()
+		let month = ref(date.getMonth() + 1)
+		let year = ref(date.getFullYear())
 		let finances = ref([])
 		let fields = ['Date', 'Description', 'Income', 'Expense', 'Balance', '']
 		const record = reactive({
@@ -20,9 +23,17 @@ export default {
 			getAll()
 		})
 
+		watch(month, () => {
+			getAll()
+		})
+
+		watch(year, () => {
+			getAll()
+		})
+
 		async function getAll() {
 			await axios
-				.get('/api/finance')
+				.get(`/api/finance/report/${month.value}/${year.value}`)
 				.then((result) => {
 					finances.value = result.data
 				})
@@ -68,11 +79,14 @@ export default {
 		}
 
 		return {
+			month,
+			year,
 			finances,
 			fields,
 			record,
 			toIdr,
 			create,
+			getAll,
 		}
 	},
 	components: { Input, Button },
@@ -91,7 +105,7 @@ export default {
 						<select
 							class="w-full rounded border-0 bg-white p-3 text-sm text-slate-600 placeholder-slate-300 shadow transition-all duration-150 ease-linear focus:outline-none focus:ring"
 							v-model="record.type"
-                     placeholder="Type"
+							placeholder="Type"
 						>
 							<option value="income">Income</option>
 							<option value="expense">Expense</option>
@@ -103,6 +117,43 @@ export default {
 					<div class="flex flex-wrap items-center">
 						<div class="relative flex w-full max-w-full flex-grow items-center justify-between px-4">
 							<h3 class="bg-white text-lg font-semibold">Finance table</h3>
+							<div class="flex w-1/4 gap-2">
+								<select
+									class="w-full rounded border-0 bg-white p-3 text-sm text-slate-600 placeholder-slate-300 shadow transition-all duration-150 ease-linear focus:outline-none focus:ring"
+									v-model="month"
+									placeholder="Month"
+								>
+									<option value="1">January</option>
+									<option value="2">February</option>
+									<option value="3">March</option>
+									<option value="4">April</option>
+									<option value="5">May</option>
+									<option value="6">June</option>
+									<option value="7">July</option>
+									<option value="8">August</option>
+									<option value="9">September</option>
+									<option value="10">October</option>
+									<option value="11">November</option>
+									<option value="12">December</option>
+								</select>
+
+								<select
+									class="w-full rounded border-0 bg-white p-3 text-sm text-slate-600 placeholder-slate-300 shadow transition-all duration-150 ease-linear focus:outline-none focus:ring"
+									v-model="year"
+									placeholder="Year"
+								>
+									<option value="2021">2021</option>
+									<option value="2022">2022</option>
+									<option value="2023">2023</option>
+									<option value="2024">2024</option>
+									<option value="2025">2025</option>
+									<option value="2026">2026</option>
+									<option value="2027">2027</option>
+									<option value="2028">2028</option>
+									<option value="2029">2029</option>
+									<option value="2030">2030</option>
+								</select>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -112,7 +163,8 @@ export default {
 							<tr>
 								<th
 									class="whitespace-nowrap border border-l-0 border-r-0 border-solid border-slate-100 bg-slate-50 px-6 py-3 text-left align-middle text-xs font-semibold uppercase text-slate-500"
-									v-for="field in fields"
+									v-for="(field, index) in fields"
+									:key="index"
 								>
 									{{ field }}
 								</th>
